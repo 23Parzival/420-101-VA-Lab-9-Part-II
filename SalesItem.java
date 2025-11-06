0f51d19076e91bfbe4a8db616948fd6c4a680a5e
23Parzival <156597085+23Parzival@users.noreply.github.com>
2025-11-06 15:10:08 -0500

    Lab 9, part II, #21.

--- SalesItem.java
+++ SalesItem.java
@@ -117,9 +117,12 @@ public class SalesItem
         System.out.println("Price: " + priceString(price));
         System.out.println();
         System.out.println("Customer comments:");
-        for(Comment comment : comments) {
+        int i = 0;
+        while (i < comments.size()) {
+            Comment comment = comments.get(i);
             System.out.println("-------------------------------------------");
             System.out.println(comment.getFullDetails());
+            i++;
         }
         System.out.println();
         System.out.println("===========================================");
@@ -132,15 +135,21 @@ public class SalesItem
      */
     public Comment findMostHelpfulComment()
     {
-        Iterator<Comment> it = comments.iterator();
-        Comment best = it.next();
-        while(it.hasNext()) {
-            Comment current = it.next();
-            if(current.getVoteCount() > best.getVoteCount()) {
-                best = current;
-            }
+        if(comments.isEmpty()){
+            return null;
         }
-        return best;
+        else {
+            int i = 1;
+            Comment best = comments.get(0);
+            while (i < comments.size()) {
+                Comment current = comments.get(i);
+                if (current.getVoteCount() > best.getVoteCount()) {
+                    best = current;
+                }
+                i++;
+            }
+            return best; 
+        }
     }
     
     /**
@@ -158,9 +167,17 @@ public class SalesItem
      */
     private Comment findCommentByAuthor(String author)
     {
-        for(Comment comment : comments) {
-            if(comment.getAuthor().equals(author)) {
-                return comment;
+        if(comments.isEmpty()){
+            return null;
+        }
+        else {
+            int i = 0;
+            while (i < comments.size()) {
+                Comment commentWithAuthor = comments.get(i);
+                if (commentWithAuthor.getAuthor().equals(author)) {
+                    return commentWithAuthor;
+                }
+                i++;
             }
         }
         return null;
